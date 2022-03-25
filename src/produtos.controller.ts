1eef50bb68f82aa7e623b759dcda99aed97ea2b0
Airton Araujo Sousa <airton.araujo@rarolabs.com.br>
2022-03-25 17:43:53 -0300

    endpoints for Products

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 
 @Controller('produtos')
 export class ProdutosController{
   @Get()
   obterTodos(): string{
       return 'Lista de todos os produtos'
+  }
+
+  @Get(':id')
+  obterUm(@Param() params): string{
+      return `Retorna os dados do produto ${params.id}`
+  }
+
+  @Post()
+  criar(@Body() produto): string{
+      return 'Produto criado';
+  }
+
+  @Put()
+  atualizar(@Body() produto): string{
+      return 'Produto criado';
+  }
+
+  @Delete(':id')
+  delete(@Param() params): string{
+    return `Apaga o produto ${params.id}`;
   }
 }
